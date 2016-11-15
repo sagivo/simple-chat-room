9417d0d5595d413068ef2f2df91e530bb6529f1b
Sagiv Ofek <sagiv4@gmail.com>
2016-11-14 22:42:01 -0500

    a

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+const port = 8080;
+app.listen(port, function () {
+  console.log(`Example app listening on port ${ port }`)
+})
